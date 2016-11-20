f872022e393c781156bcd947af024764b84eea07
tanysaur <tanysaur@gmail.com>
2016-11-19 18:02:51 -0600

    added real trivia q&a

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,6 +8,7 @@
 // Don't forget to include a countdown timer.
 $(document).ready(function() {
 
+//Variable declaration
 	var wins = 0;
 	var losses = 0;
 	var q = 0;
@@ -15,22 +16,65 @@ $(document).ready(function() {
 
 	var trivia = [
 		{
-			question: "Question text here 00",
-			answer : "a0",
-			options : ["a0","a1","a2"],
+			question: "Which actor played the 11th doctor?",
+			answer : "Matt Smith",
+			options : ["Billie Piper","Matt Smith","David Tennant","Benedict Cumberbatch"],
 		},
 
 		{
-			question : "Question text here 11",
-			answer : "b1",
-			options : ["b0","b1","b2"],
+			question : "Who said this line, 'Hello Sweetie.'",
+			answer : "River Song",
+			options : ["Amy Pond","Dalek","River Song","11th Doctor"],
 		},
 
 		{
-			question : "Question text here 22",
-			answer : "c2",
-			options : ["c0","c1","c2"],
+			question : "Who is fighting in the time war?",
+			answer : "Dalek & the Timelords",
+			options : ["The Master & the Timelords","The Master & the Timelords","Dalek & the Timelords","Cybermen & the Timelords"],
+		},
+
+		{
+			question : "Who is the Doctor's arch enemy?",
+			answer : "Daleks",
+			options : ["The Timelords","The Master","Daleks","Cybermen"],
+		},
+
+		{
+			question : "What does TARDIS stand for?",
+			answer : "Time And Relative Dimension In Space",
+			options : ["Timely Annoying Divergent Interesting Sports","Totally Awesome Rhinos Digging In Spain","Time And Relative Dimension In Space","Time And Realm Dimension In Space"],
+		},
+
+		{
+			question : "How old is the Doctor?",
+			answer : "Over 2000",
+			options : ["Over 2000","Under 100","5 billion","3.1415 million"],
+		},
+
+		{
+			question : "What happens if a weeping angel touches you?",
+			answer : "You get set back in time",
+			options : ["You die","You turn into one","You get set back in time","You start weeping uncontrollably"],
+		},
+
+		{
+			question : "What is the Dalek's catch phrase?",
+			answer : "Exterminate!",
+			options : ["Die!","Exfoliate!","Delete!","Exterminate!"],
+		},
+
+		{
+			question : "Which Doctor has a scarf?",
+			answer : "Fourth Doctor",
+			options : ["Fourth Doctor","9th Doctor","First Doctor","12th Doctor"],
+		},
+
+		{
+			question : "What was Clara Oswald's favorite dish to make?",
+			answer : "Souffle",
+			options : ["Pie","Souffle","Creme brulee","Custard"],
 		}
+
 	];
 
 	var stopwatch = {
@@ -82,7 +126,13 @@ $(document).ready(function() {
 	//     //game.nextQ;
 	    
 	// };
-	
+
+	$('#startButton').click(function(){
+	  $('#quiz').slideUp();
+	  $(this).hide();
+	  $(".btn-primary").css("cssText", "display: block !important; margin:auto; width: 30%;");
+	});
+
 
 	nextQ();
 
@@ -96,12 +146,22 @@ $(document).ready(function() {
  		$("#question").html("<h1>"+trivia[q].question+ "</h1>");
 
  		//add buttons from trivia object
+ 		var answerDiv = $('<div class="answers text-center">');
+ 		$("#question").after(answerDiv);
 		for(i = 0; i < trivia[q].options.length; i++){
-			$("#answers").append("<button class='btn-primary buttonOptions'>" + trivia[q].options[i] + "</button>" + "<br>");
+			$(".choices").append("<input type='radio' name='choice'" + "value='"+ i + "'>" + trivia[q].options[i] + "\n");
 		}
 		
 	}
+
 	
+	//when user clicks on a button, 
+	$(radio).on("click", function(){
+		var myClick = $(this).text();
+		console.log(myClick);
+		userChoice = myClick;
+		checkAnswer();
+	});
 
 	//check if user choice is correct
 	function checkAnswer(){
@@ -112,24 +172,18 @@ $(document).ready(function() {
 			$("#wins").html("Correct: "+wins);
 			nextQ();
 			q++;
+			return true;
 		}else{
 			console.log("incorrect");
 			losses++;
 			$("#losses").html("Inorrect: "+losses);
 			q++;
+			return false;
+			//add fun facts
 		}
-		$("#answers").empty();
+		$(".answers").remove();
 		nextQ();
 	}
-
-
-	//when user clicks on a button, 
-	$(".buttonOptions").on("click", function(){
-		var myClick = $(this).text();
-		console.log(myClick);
-		userChoice = myClick;
-		checkAnswer();
-	});
 
 });
 
